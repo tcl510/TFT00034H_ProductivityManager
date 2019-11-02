6a6fed1de46b0253ee8da29101c95b2637591fb6
Arthur <tcl510@york.ac.uk>
2019-11-02 09:33:15 +0000

    Initial commit

--- app/src/main/java/y3860172/york/ac/uk/tft00034h_productivitymanager/MainActivity.java
+++ app/src/main/java/y3860172/york/ac/uk/tft00034h_productivitymanager/MainActivity.java
@@ -1,7 +1,7 @@
 package y3860172.york.ac.uk.tft00034h_productivitymanager;
 
 import androidx.appcompat.app.AppCompatActivity;
-//import androidx.cardview.widget.CardView;
+//import androidx.cardview.widget;
 
 import android.os.Bundle;
 
@@ -11,5 +11,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        //get weather data based on gps
+        //get calander data
+        initialize();
     }
+
+    private void initialize(){
+
+    }
+
+
 }
