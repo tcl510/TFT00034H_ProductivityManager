cb2f487c03d76c2c6e7691af7c37884ecbdefa32
Arthur <tcl510@york.ac.uk>
2019-11-02 07:59:14 +0000

    Initial commit

---
+++ app/src/main/java/y3860172/york/ac/uk/tft00034h_productivitymanager/MainActivity.java
@@ -0,0 +1,15 @@
+package y3860172.york.ac.uk.tft00034h_productivitymanager;
+
+import androidx.appcompat.app.AppCompatActivity;
+//import androidx.cardview.widget.CardView;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
