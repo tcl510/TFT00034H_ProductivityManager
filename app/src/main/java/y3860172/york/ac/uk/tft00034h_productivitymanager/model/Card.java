f8ce65d6fe462513c58198c07cc94a4e4cebce3d
Arthur <tcl510@york.ac.uk>
2019-11-02 12:26:47 +0000

    trying stuff out

---
+++ app/src/main/java/y3860172/york/ac/uk/tft00034h_productivitymanager/model/Card.java
@@ -0,0 +1,16 @@
+package y3860172.york.ac.uk.tft00034h_productivitymanager.model;
+
+import android.media.Image;
+
+public class Card {
+    public String title = "Default Title";
+    public String subtitle = "Default Subtitle goes here, more words, words and more words";
+    public String supporting = "a subtitle to behold!";
+    public String avatar, media;
+
+
+
+    Card (){
+        avatar =
+    }
+}
