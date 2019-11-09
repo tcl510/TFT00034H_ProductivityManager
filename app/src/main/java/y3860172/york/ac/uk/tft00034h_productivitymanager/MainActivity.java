cb485a99567a2ebd1c6286eda1e31c1fa0c5d470
Arthur <tcl510@york.ac.uk>
2019-11-09 23:37:10 +0000

    Implimented card system with placeholder images, ironing out bugs then will impliment actual data

--- app/src/main/java/y3860172/york/ac/uk/tft00034h_productivitymanager/MainActivity.java
+++ app/src/main/java/y3860172/york/ac/uk/tft00034h_productivitymanager/MainActivity.java
@@ -1,16 +1,42 @@
 package y3860172.york.ac.uk.tft00034h_productivitymanager;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 //import androidx.cardview.widget;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import y3860172.york.ac.uk.tft00034h_productivitymanager.adapter.cardAdapter;
+import y3860172.york.ac.uk.tft00034h_productivitymanager.model.Card;
+
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView mRecycleView;
+    private cardAdapter mAdapter;
+    private List<Card> mCardList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //binding recycleview
+        mRecycleView = (RecyclerView) findViewById(R.id.idRecycleView);
+        //fix size
+        mRecycleView.setLayoutManager(new LinearLayoutManager(this));
+
+        //populate
+        mCardList = new ArrayList<>();
+        mCardList.add(new Card ("Default Title", "Default Subtitle goes here, more words, words and more words", "a subtitle to behold!", R.drawable.tedted, R.drawable.tedted));
+        mCardList.add(new Card ("Default Title", "Default Subtitle goes here, more words, words and more words", "a subtitle to behold!", R.drawable.tedted, R.drawable.tedted));
+
+        //set adapter to recycleview
+        mAdapter = new cardAdapter(mCardList,this);
+        mRecycleView.setAdapter(mAdapter);
 
 
         //get weather data based on gps
@@ -22,5 +48,6 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+//    Card tester = new Card ("Default Title", "Default Subtitle goes here, more words, words and more words", "a subtitle to behold!", "tedted.jpg", "tedted.jpg");
 
 }
--- app/src/main/java/y3860172/york/ac/uk/tft00034h_productivitymanager/adapter/cardAdapter.java
+++ app/src/main/java/y3860172/york/ac/uk/tft00034h_productivitymanager/adapter/cardAdapter.java
@@ -1,7 +1,77 @@
 package y3860172.york.ac.uk.tft00034h_productivitymanager.adapter;
 
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
 import androidx.recyclerview.widget.RecyclerView;
 
-//public class cardAdapter extends RecyclerView.Adapter {
-//}
-//GET THIS TO WORK! https://iteritory.com/android-cardview-tutorial-with-example/
+import org.w3c.dom.Text;
+
+import java.util.List;
+
+import y3860172.york.ac.uk.tft00034h_productivitymanager.R;
+import y3860172.york.ac.uk.tft00034h_productivitymanager.model.Card;
+
+public class cardAdapter extends RecyclerView.Adapter<cardAdapter.cardViewHolder>{
+    private List<Card> infoList;
+    Context context;
+
+    public cardAdapter(List<Card> infoList, Context context ){
+        this.infoList = infoList;
+        this.context = context;
+    }
+
+    @Override
+    public cardViewHolder onCreateViewHolder(ViewGroup parent, int viewType){
+        //inflating the file
+        View cardView = LayoutInflater.from(parent.getContext()).inflate(R.layout.card, parent, false);
+        cardViewHolder gvh = new cardViewHolder(cardView);
+        return gvh;
+    }
+    @Override
+    public void onBindViewHolder(cardViewHolder holder, final int position){
+        holder.txtTitle.setText(infoList.get(position).getTitle());
+        holder.txtSubtitle.setText(infoList.get(position).getSubtitle());
+        holder.txtSupporting.setText(infoList.get(position).getSupporting());
+        holder.imgAvatar.setImageResource(infoList.get(position).getAvatar());
+        holder.imgMedia.setImageResource(infoList.get(position).getMedia());
+
+    }
+
+    @Override
+    public int getItemCount(){
+        return infoList.size();
+    }
+
+    public class cardViewHolder extends RecyclerView.ViewHolder {
+        TextView txtTitle;
+        TextView txtSubtitle;
+        TextView txtSupporting;
+        ImageView imgAvatar;
+        ImageView imgMedia;
+        public cardViewHolder (View view){
+            super(view);
+            txtTitle = view.findViewById(R.id.title_text);
+            txtSubtitle = view.findViewById(R.id.subtitle_text);
+            txtSupporting = view.findViewById(R.id.supporting_text);
+            imgAvatar = view.findViewById(R.id.avatar_image);
+            imgMedia = view.findViewById(R.id.media_image);
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+//credits to https://iteritory.com/android-cardview-tutorial-with-example/
+//cards system based on android materials
