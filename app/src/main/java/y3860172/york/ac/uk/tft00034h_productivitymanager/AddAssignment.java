6f4b7de5d18f34d53023e45fafc330d2e1f2d9de
Arthur <tcl510@york.ac.uk>
2019-12-03 11:17:45 +0000

    adjusted layout, started to impliment media lists

--- app/src/main/java/y3860172/york/ac/uk/tft00034h_productivitymanager/AddAssignment.java
+++ app/src/main/java/y3860172/york/ac/uk/tft00034h_productivitymanager/AddAssignment.java
@@ -1,11 +1,12 @@
 package y3860172.york.ac.uk.tft00034h_productivitymanager;
 
 import android.os.Bundle;
-import android.transition.TransitionManager;
 import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.transition.AutoTransition;
+import androidx.transition.TransitionManager;
 
 public class AddAssignment extends AppCompatActivity {
 
@@ -16,7 +17,8 @@ public class AddAssignment extends AppCompatActivity {
     }
 
     public void onClickToggleDate(View v){
-        TransitionManager.beginDelayedTransition((ViewGroup) v);
+        TransitionManager.beginDelayedTransition((ViewGroup) v.getParent(), new AutoTransition());
+
         VisExpander(findViewById(R.id.datePicker));
         VisDisable(findViewById(R.id.TimePicker));
     }
@@ -25,16 +27,21 @@ public class AddAssignment extends AppCompatActivity {
         VisDisable(findViewById(R.id.datePicker));
     }
     public void VisExpander(View picker){
+//        TransitionManager.beginDelayedTransition((ViewGroup) picker.getParent().getParent(), new AutoTransition());
 
         if (picker.getVisibility() == View.GONE){
             picker.setVisibility(View.VISIBLE);
         } else {
+
             picker.setVisibility(View.GONE);
         }
     }
     public void VisDisable(View picker){
+//        TransitionManager.beginDelayedTransition((ViewGroup) picker.getParent().getParent(), new AutoTransition());
+
         picker.setVisibility(View.GONE);
     }
     //todo add time and date updates to tags
     //todo make the pickers start at current time
+    //todo make transitions
 }
